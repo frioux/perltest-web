25d7829fc924cd3bc16d362d7d0ddf9b24e44fde
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2009-05-07 00:24:19 -0500

    use current dir and don't reinstantiate tester

--- bin/webserver.pl
+++ bin/webserver.pl
@@ -4,13 +4,17 @@ use warnings;
 use CGI::Application::Server;
 use FindBin;
 use lib "$FindBin::Bin/../lib";
-use WebTest::Dispatch;
+use WebTest::Controller;
+use Readonly;
+Readonly my $port => shift || 5052;
+Readonly my $dir  => shift || q{.};
 
-my $server = CGI::Application::Server->new(5052);
+my $server = CGI::Application::Server->new($port);
+my $app    = WebTest::Controller->new( PARAMS => {
+   dir => $dir
+});
 
-$server->entry_points({
-      '/tests'     => 'WebTest::Dispatch',
-      '/static'    => '/',
-   });
+$server->entry_points({ '/tests' => $app });
 $server->document_root("$FindBin::Bin/../static");
 $server->run();
+
--- lib/WebTest/Controller.pm
+++ lib/WebTest/Controller.pm
@@ -2,12 +2,20 @@ package WebTest::Controller;
 use strict;
 use warnings;
 use base 'CGI::Application';
-use feature ':5.10';
 use CGI::Application::Plugin::AutoRunmode;
 use WebTest::Tester;
 
-sub main : StartRunmode {
+sub tester {
    my $self = shift;
+   if ( !$self->param('tester') ) {
+      $self->param( 'tester',
+         WebTest::Tester->new( { directory => $self->param('dir') } ) );
+   }
+   return $self->param('tester');
+}
+
+sub main : StartRunmode {
+   my $self   = shift;
    my $output = <<'HTML';
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
@@ -18,8 +26,7 @@ sub main : StartRunmode {
 <body><pre>
 HTML
 
-   my $tester = WebTest::Tester->new( { directory => '/home/frew/personal/web_test' } );
-   $output .= $tester->tests;
+   $output .= $self->tester->tests;
    $output .= <<'HTML';
 </pre>
 </body>
